a74cb915d80cc8b651e66dce288ca9a7511f8465
skysh6666 <skysh66666@gmail.com>
2025-07-30 19:01:41 +0800

    Create init.lua
    首次上传,v1.0

---
+++ BTE-Btl Text Editor/init.lua
@@ -0,0 +1,16 @@
+appname="BTE-Btl Text Editor"
+appver="1.0"
+appcode="1"
+appsdk="15"
+path_pattern=""
+packagename="com.bte.wc4"
+theme="Theme_Material"
+app_key=""
+app_channel=""
+developer=""
+description=""
+debugmode=false
+user_permission={
+  "INTERNET",
+ "WRITE_EXTERNAL_STORAGE"
+}
